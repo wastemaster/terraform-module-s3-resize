4ffeacda8032a26dd99015b02cb664217eaf5b14
Michael Olšavský <molsavsky1@gmail.com>
2019-07-12 15:05:42 +0200

    change lambda file path to support modules

--- main.tf
+++ main.tf
@@ -281,11 +281,11 @@ resource "aws_iam_policy_attachment" "access" {
 }
 
 resource "aws_lambda_function" "lambda" {
-  filename         = "./lambda.zip"
+  filename         = "${path.module}/lambda.zip"
   function_name    = "${var.project_name}-${var.environment}-resize"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "index.handler"
-  source_code_hash = "${base64sha256(file("./lambda.zip"))}"
+  source_code_hash = "${base64sha256(file("${path.module}/lambda.zip"))}"
   runtime          = "nodejs8.10"
   memory_size      = 1536
   timeout          = 30
